0416c1d4a1719d023095add44618907201b2dfa2
mikegsCoder <mikegsid@gmail.com>
2025-03-29 09:02:51 +0200

    Add event listeners

--- WeatherApp-with-Lit-HTML/src/app.js
+++ WeatherApp-with-Lit-HTML/src/app.js
@@ -8,3 +8,12 @@ let city = '';
 document.body.classList.add('light');
 
 const root = document.getElementById('root');
+
+// add event listeners:
+document.getElementById('checkbox').addEventListener('click', toggleTheme);
+document.getElementById('input').addEventListener('input', handleInputChange);
+document.getElementById('btn').addEventListener('click', handleSearch);
+
+function handleInputChange(e) {}
+
+async function handleSearch() {}
