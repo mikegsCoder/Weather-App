ff8109ed230475825f5e7179bc907a4ff51fa497
mikegsCoder <mikegsid@gmail.com>
2025-04-01 07:22:21 +0300

    Add createBodyData

--- WeatherApp-with-Lit-HTML/src/utils/util.js
+++ WeatherApp-with-Lit-HTML/src/utils/util.js
@@ -7,3 +7,32 @@ export function toggleTheme() {
     document.body.classList.add('dark');
   }
 };
+
+export function createBodyData(data) {
+  const pressure = {
+    icon: 'fa-solid fa-gauge-high',
+    value: data?.main?.pressure,
+    units: 'hPa',
+    text: 'Pressure'
+  };
+  const humidity = {
+    icon: 'fa-solid fa-droplet',
+    value: data?.main?.humidity,
+    units: '%',
+    text: 'Humidity'
+  };
+  const visibility = {
+    icon: 'fa-solid fa-eye',
+    value: data?.visibility / 1000,
+    units: 'km',
+    text: 'Visibility'
+  };
+
+  const bodyData = [
+    pressure, 
+    humidity, 
+    visibility, 
+  ];
+
+  return bodyData;
+}
