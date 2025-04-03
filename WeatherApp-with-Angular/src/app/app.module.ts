facb3049a4823fcc6121a2ad8da1cbdd022fb3a7
mikegsCoder <mikegsid@gmail.com>
2025-04-03 07:40:05 +0300

    Add Weather app with Angular initial commit

---
+++ WeatherApp-with-Angular/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [],
+  providers: [],
+  bootstrap: [],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+})
+export class AppModule {}
