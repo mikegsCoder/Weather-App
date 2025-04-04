7f8920cdcd133646ea4c01391acae7f50f4afcfd
mikegsCoder <mikegsid@gmail.com>
2025-04-04 08:41:07 +0300

    Add appConstants

---
+++ WeatherApp-with-Angular/src/app/shared/constants/appConstants.ts
@@ -0,0 +1,15 @@
+const apiKey: string =  'Your-API-Key';
+const baseUrl: string =  'https://api.openweathermap.org/data/2.5/';
+const sourceCodeUrl: string = 'https://github.com/mikegsCoder/';
+const dataProviderUrl: string = 'https://openweathermap.org/';
+const angularUrl: string = 'https://angular.dev/';
+const logo: string = '../../../assets/img/logo.gif';
+
+export {
+  apiKey,
+  baseUrl,
+  sourceCodeUrl,
+  dataProviderUrl,
+  angularUrl,
+  logo
+};
