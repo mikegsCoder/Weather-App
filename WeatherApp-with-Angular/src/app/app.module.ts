e640ad5b693aa3d2dcf68a4378e574b6c9e27937
mikegsCoder <mikegsid@gmail.com>
2025-04-04 08:45:04 +0300

    Add CoreModule

--- WeatherApp-with-Angular/src/app/app.module.ts
+++ WeatherApp-with-Angular/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { CoreModule } from './core/core.module';
+
 @NgModule({
   declarations: [],
   providers: [],
   bootstrap: [],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule
   ],
 })
 export class AppModule {}
