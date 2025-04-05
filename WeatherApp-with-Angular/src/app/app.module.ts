72ef405ff700bf76a9c30df2442702c58ae4edc3
mikegsCoder <mikegsid@gmail.com>
2025-04-05 10:48:18 +0300

    Add HeaderComponent

--- WeatherApp-with-Angular/src/app/app.module.ts
+++ WeatherApp-with-Angular/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { HeaderComponent } from './core/header/header.component';
+
 import { BodyModule } from './body/body.module';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [],
   providers: [],
-  bootstrap: [],
+  bootstrap: [
+    HeaderComponent
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
