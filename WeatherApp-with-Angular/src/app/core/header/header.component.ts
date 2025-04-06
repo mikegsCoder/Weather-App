52d9236cd7b18f997c4a2431d0ae92dc39a35b85
mikegsCoder <mikegsid@gmail.com>
2025-04-06 09:57:51 +0300

    Implemented toggleTheme

--- WeatherApp-with-Angular/src/app/core/header/header.component.ts
+++ WeatherApp-with-Angular/src/app/core/header/header.component.ts
@@ -20,5 +20,13 @@ export class HeaderComponent implements OnInit {
     document.body.classList.add('light');
   }
 
-  toggleTheme = (): void => {};
+  toggleTheme = (): void => {
+    if (document.body.classList.contains('dark')) {
+      document.body.classList.remove('dark');
+      document.body.classList.add('light');
+    } else if (document.body.classList.contains('light')) {
+      document.body.classList.remove('light');
+      document.body.classList.add('dark');
+    }
+  };
 }
