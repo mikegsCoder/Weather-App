de0d57d50931ee130aeffbb383c149bac3e5f049
mikegsCoder <mikegsid@gmail.com>
2025-04-13 09:56:56 +0300

    Add getBodyData

--- WeatherApp-with-Angular/src/app/body/weather.service.ts
+++ WeatherApp-with-Angular/src/app/body/weather.service.ts
@@ -6,6 +6,7 @@ import { apiKey } from '../shared/constants/appConstants';
 
 import IResponseData from '../shared/interfaces/IResponseData';
 import IGeneralData from '../shared/interfaces/IGeneralData';
+import IBodyData from '../shared/interfaces/IBodyData';
 
 @Injectable()
 export class WeatherService {
@@ -32,5 +33,55 @@ export class WeatherService {
       temp: ~~(data.main.temp - 273.15).toFixed(2),
       feels_like: ~~(data.main.feels_like - 273.15).toFixed(2) - 1
     }
+  }
+
+  getBodyData(data: IResponseData): IBodyData[] {
+    const pressure: IBodyData = {
+      icon: 'fa-solid fa-gauge-high',
+      value: data?.main?.pressure,
+      units: 'hPa',
+      text: 'Pressure'
+    };
+    const humidity: IBodyData = {
+      icon: 'fa-solid fa-droplet',
+      value: data?.main?.humidity,
+      units: '%',
+      text: 'Humidity'
+    };
+    const visibility: IBodyData = {
+      icon: 'fa-solid fa-eye',
+      value: data?.visibility / 1000,
+      units: 'km',
+      text: 'Visibility'
+    };
+    const windSpeed: IBodyData = {
+      icon: 'fa-solid fa-wind',
+      value: data?.wind?.speed,
+      units: 'km/h',
+      text: 'Wind Speed'
+    };
+    const windDirection: IBodyData = {
+      icon: 'fa-solid fa-compass',
+      value: data?.wind?.deg,
+      units: 'deg',
+      text: 'Wind Direction'
+    };
+    const cloudiness: IBodyData = {
+      icon: 'fa-solid fa-cloud',
+      value: data?.clouds?.all,
+      units: '%',
+      text: 'Cloudiness'
+    };
+
+    const bodyData: IBodyData[] = [
+      pressure, 
+      humidity, 
+      visibility, 
+      windSpeed, 
+      windDirection, 
+      cloudiness
+    ];
+
+    return bodyData;
   }
 }
