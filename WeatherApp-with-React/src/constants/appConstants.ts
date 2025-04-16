3f65617e653bf1811ab089b94a5203b76717ec5f
mikegsCoder <mikegsid@gmail.com>
2025-04-16 08:14:25 +0300

    Add application constants

---
+++ WeatherApp-with-React/src/constants/appConstants.ts
@@ -0,0 +1,13 @@
+const apiKey: string =  'Your-API-Key';
+const baseUrl: string =  'https://api.openweathermap.org/data/2.5/';
+const sourceCodeUrl: string = 'https://github.com/mikegsCoder/Weather-App/';
+const dataProviderUrl: string = 'https://openweathermap.org/';
+const reactTSUrl: string = 'https://react.dev/learn/typescript/';
+
+export {
+  apiKey,
+  baseUrl,
+  sourceCodeUrl,
+  dataProviderUrl,
+  reactTSUrl
+};
