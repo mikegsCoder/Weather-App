3c0851472249bed9c30aa0fcc402e829141b1087
mikegsCoder <mikegsid@gmail.com>
2025-04-17 09:52:55 +0300

    Add axios instance

---
+++ WeatherApp-with-React/src/dataProviders/axios.ts
@@ -0,0 +1,6 @@
+import axios, { AxiosInstance } from 'axios';
+import { baseUrl } from '../constants/appConstants';
+
+export const axiosInstance: AxiosInstance = axios.create({
+  baseURL: baseUrl
+});
