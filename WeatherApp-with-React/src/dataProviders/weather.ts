d8b58bae5973d25de5d2a3af9dec0907dc8eceea
mikegsCoder <mikegsid@gmail.com>
2025-04-17 09:54:30 +0300

    Add weather data provider

---
+++ WeatherApp-with-React/src/dataProviders/weather.ts
@@ -0,0 +1,19 @@
+import { axiosInstance } from './axios';
+import { AxiosResponse } from 'axios';
+
+import { apiKey } from '../constants/appConstants';
+import IResponseData from '../interfaces/IResponseData';
+
+export async function getWeatherData(cityName: string): Promise<IResponseData | null> {
+  const apiURL: string = `weather?q=${cityName}&appid=${apiKey}`;
+
+  try {
+    const res: AxiosResponse<any, any> = await axiosInstance.get(apiURL);
+    console.log("response: ", res.data);
+    return res.data;
+  }
+  catch (err) {
+    console.log("err: ", err);
+    return null;
+  }
+}
