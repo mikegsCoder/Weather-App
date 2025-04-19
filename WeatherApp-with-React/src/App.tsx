04de350875aeb069d021107bcc69658fe4843e8d
mikegsCoder <mikegsid@gmail.com>
2025-04-19 09:18:45 +0300

    Add Header component

--- WeatherApp-with-React/src/App.tsx
+++ WeatherApp-with-React/src/App.tsx
@@ -1,11 +1,16 @@
 import "./assets/css/styles.css";
 
+import logo from "./assets/img/logo.gif";
+import { reactTSUrl } from "./constants/appConstants";
+
 import ErrorBoundary from "./components/ErrorBoundary/";
+import Header from "./components/Header";
 
 function App(): JSX.Element {
 
   return (
     <ErrorBoundary>
+      <Header logo={logo} reactTSUrl={reactTSUrl} />
       <h1>Weather App with React</h1>
     </ErrorBoundary>
   );
---
+++ WeatherApp-with-React/src/components/Header/Header.tsx
@@ -0,0 +1,54 @@
+import './Header.css';
+
+import Theme from '../../enumerations/theme';
+
+import { useEffect, useState } from 'react';
+import { FiMoon, FiSun } from 'react-icons/fi';
+
+const Header = (props: {
+  logo: string,
+  reactTSUrl: string
+}): JSX.Element => {
+  const [theme, setTheme] = useState(Theme[0]);
+
+  const toggleTheme = (): void => {
+    // to implement ...
+  };
+
+  useEffect((): void => {
+    document.body.className = theme;
+  }, [theme]);
+
+  return (
+    <>
+      <a
+        href={props.reactTSUrl}
+        target={"_blank"}
+        rel="noreferrer"
+        className="text-3xl flex items-center gap-2 absolute top-2 left-10 font-semibold"
+      >
+        <img src={props.logo} className="w-[120px]" id="logo" alt="logo" />
+        Weather App with React.TS
+      </a>
+      <div className="icon-button toggle-theme">
+        <input
+          type="checkbox"
+          className="checkbox"
+          id="checkbox"
+          onClick={toggleTheme}
+        />
+        <label
+          htmlFor="checkbox"
+          className="label top-2 right-10"
+        >
+          <FiMoon color="white" />
+          <FiSun color="yellow" />
+          <div className="ball"></div>
+        </label>
+      </div>
+      <hr className='hr-top' />
+    </>
+  )
+}
+
+export default Header;
