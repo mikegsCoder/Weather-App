6c45833524013fb0994c08693a4a67d5dcc707fb
mikegsCoder <mikegsid@gmail.com>
2025-04-23 07:26:05 +0300

    Add Loader

---
+++ WeatherApp-with-React/src/components/Body/Partials/Loader/Loader.tsx
@@ -0,0 +1,11 @@
+import './Loader.css';
+
+const Loader = (): JSX.Element => {
+  return (
+    <div className="wrapper">
+      <span className="loader" />
+    </div>
+  )
+}
+
+export default Loader;
