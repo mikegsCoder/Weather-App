21767062f4a386a7bcff1e2f52fbee16d39fdbc6
mikegsCoder <mikegsid@gmail.com>
2025-04-28 07:03:58 +0300

    Add application constants

---
+++ WeatherApp-with-VueJS/src/constants/appConstants.js
@@ -0,0 +1,15 @@
+const apiKey =  'Your-API-Key';
+const logo = './img/logo.gif';
+const baseUrl =  'https://api.openweathermap.org/data/2.5/';
+const sourceCodeUrl = 'https://github.com/mikegsCoder/Weather-App/';
+const dataProviderUrl = 'https://openweathermap.org/';
+const vueJSUrl = 'https://vuejs.org/';
+
+export {
+  apiKey,
+  logo,
+  baseUrl,
+  sourceCodeUrl,
+  dataProviderUrl,
+  vueJSUrl
+};
