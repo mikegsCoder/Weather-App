ec4da324719bc6afd7aad7182271e28515d1828e
mikegsCoder <mikegsid@gmail.com>
2025-04-30 07:10:38 +0300

    Implemented toggleTheme

--- WeatherApp-with-VueJS/src/components/Header.vue
+++ WeatherApp-with-VueJS/src/components/Header.vue
@@ -15,7 +15,13 @@ const props = defineProps({
 });
 
 const toggleTheme = () => {
-  // to implement
+  if (document.body.classList.contains('dark')) {
+    document.body.classList.remove('dark');
+    document.body.classList.add('light');
+  } else if (document.body.classList.contains('light')) {
+    document.body.classList.remove('light');
+    document.body.classList.add('dark');
+  }
 };
 </script>
 
