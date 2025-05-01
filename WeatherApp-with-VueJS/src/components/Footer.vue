915a490db42fa09787796e6c1724926d71ba909e
mikegsCoder <mikegsid@gmail.com>
2025-05-01 08:43:49 +0300

    Add Footer component

---
+++ WeatherApp-with-VueJS/src/components/Footer.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps({
+  data: {
+    type: Object,
+    required: true,
+    default: () => ({
+      dataProviderUrl: string,
+      sourceCodeUrl: string
+    })
+  }
+});
+</script>
+
+<template>
+  <hr class='hr-bottom' />
+  <a 
+    :href=props.data.dataProviderUrl 
+    target="_blank" 
+    rel="noreferrer"
+    class="text-3xl font-semibold absolute bottom-5 left-10">
+    Data Provider
+  </a>
+  <a 
+    :href=props.data.sourceCodeUrl 
+    target="_blank" 
+    rel="noreferrer"
+    class="text-3xl font-semibold absolute bottom-5 right-10">
+    Source Code
+  </a>
+</template>
+
+<style scoped>
+</style>
