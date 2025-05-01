3c418f88b79ecfbfa88eb4cd8d493190996eea1a
mikegsCoder <mikegsid@gmail.com>
2025-05-01 08:45:21 +0300

    Add Footer

--- WeatherApp-with-VueJS/src/App.vue
+++ WeatherApp-with-VueJS/src/App.vue
@@ -2,13 +2,17 @@
 import { 
   logo, 
   vueJSUrl, 
+  dataProviderUrl, 
+  sourceCodeUrl 
 } from "./constants/appConstants";
 
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <Header :data="{logo, vueJSUrl}"/>
+  <Footer :data="{dataProviderUrl, sourceCodeUrl}"/>
 </template>
 
 <style scoped>
