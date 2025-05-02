61840ef317fba95ccad6afe0310165f1dcf71c60
mikegsCoder <mikegsid@gmail.com>
2025-05-02 09:19:20 +0300

    Add Body component

---
+++ WeatherApp-with-VueJS/src/components/Body.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue';
+
+const city = ref('');
+const data = ref({});
+const bodyData = ref([]);
+const loading = ref(false);
+const notFound = ref(false);
+const inputChanges = ref(false);
+
+</script>
+
+<template>
+<div class="">
+  <div class="p-5 lg:p-10">
+    <p>Body</p>
+  </div>
+</div>
+</template>
+
+<style scoped>
+</style>
