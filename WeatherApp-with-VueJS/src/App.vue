331d575f12a0c7c77d8d1e00abca3511ab877740
mikegsCoder <mikegsid@gmail.com>
2025-05-02 09:20:46 +0300

    Add Body

--- WeatherApp-with-VueJS/src/App.vue
+++ WeatherApp-with-VueJS/src/App.vue
@@ -7,11 +7,13 @@ import {
 } from "./constants/appConstants";
 
 import Header from './components/Header.vue';
+import Body from './components/Body.vue';
 import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <Header :data="{logo, vueJSUrl}"/>
+  <Body />
   <Footer :data="{dataProviderUrl, sourceCodeUrl}"/>
 </template>
 
