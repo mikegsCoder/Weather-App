75156485f3f1a8607b7b444aaa89f673611b1d59
mikegsCoder <mikegsid@gmail.com>
2025-05-05 08:54:52 +0300

    Add NotFound component

---
+++ WeatherApp-with-VueJS/src/components/partials/NotFound.vue
@@ -0,0 +1,31 @@
+<script setup>
+const props = defineProps({
+  data: {
+    type: Object,
+    required: true,
+    default: () => ({
+      city: string
+    })
+  }
+})
+</script>
+
+<template>
+<div 
+  className="border-2 rounded-lg p-2 flex text-center items-center justify-center gap-2 rounded-lg text-4xl"
+  id="not-found"
+>
+  <i class="fa-regular fa-circle-xmark"></i>
+  <div>
+    <h3 className="flex text-center items-center justify-center gap-2 rounded-lg text-4xl">
+      Invalid city name provided: <strong>{{props.data.city}}</strong>
+    </h3>
+    <h3 className="flex text-center items-center justify-center gap-2 rounded-lg text-3xl">
+      Please enter a valid city name!
+    </h3>
+  </div>
+</div>
+</template>
+
+<style scoped>
+</style>
