57d17f4001bf3140dbb938acf98d8f11b5755f17
mikegsCoder <mikegsid@gmail.com>
2025-05-06 15:36:19 +0300

    Add GeneralInfo

--- WeatherApp-with-VueJS/src/components/Body.vue
+++ WeatherApp-with-VueJS/src/components/Body.vue
@@ -4,6 +4,7 @@ import { ref } from 'vue';
 import CityInput from './partials/CityInput.vue';
 import Loader from './partials/Loader.vue';
 import NotFound from './partials/NotFound.vue';
+import GeneralInfo from './partials/GeneralInfo.vue';
 
 const city = ref('');
 const data = ref({});
@@ -27,8 +28,15 @@ const handleInputChange = () => {
     <CityInput @search="handleSearch" @input="handleInputChange" />
     <Loader v-if="loading"/>
     <NotFound v-if="!loading && !inputChanges && notFound" :data="{city}"/>
-    
-    <p>Body</p>
+    <div 
+      v-if="!loading && !inputChanges && !notFound && Object.keys(data).length > 0" 
+      class="text-center" role="status"
+    >
+      <div class="grid ">
+        <GeneralInfo :data="data" :key="data.cod"/>
+        <!-- To add info cards -->
+      </div>
+    </div>
   </div>
 </div>
 </template>
