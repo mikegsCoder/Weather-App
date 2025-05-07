ea57c1abd480895c2cb210b8036ac1537c29529d
mikegsCoder <mikegsid@gmail.com>
2025-05-07 06:54:18 +0300

    Add InfoCard component

---
+++ WeatherApp-with-VueJS/src/components/partials/InfoCard.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({
+  data: {
+    type: Object,
+    required: true,
+    default: () => ({
+      icon: string,
+      value: number,
+      units: string,
+      text: string
+    })
+  }
+})
+</script>
+
+<template>
+<div class="border-2 rounded-lg p-2">
+  <h3 class="flex text-center items-center justify-center gap-2 rounded-lg text-2xl">
+    <i :class="props.data.icon"></i>
+    {{props.data.text + ': '}}
+    {{props.data.value + '' + props.data.units}}
+  </h3>
+</div>
+</template>
+
+<style scoped>
+</style>
