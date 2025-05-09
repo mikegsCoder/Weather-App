e66782ea2cec05bbc01c1b152177b318c64f4910
mikegsCoder <mikegsid@gmail.com>
2025-05-09 07:05:39 +0300

    Add toggleTheme()

--- WeatherApp-with-ASP/WeatherApp.Web/wwwroot/js/site.js
+++ WeatherApp-with-ASP/WeatherApp.Web/wwwroot/js/site.js
@@ -3,3 +3,14 @@
 
 // Write your JavaScript code.
 
+document.body.classList.add('light');
+
+function toggleTheme() {
+    if (document.body.classList.contains('dark')) {
+        document.body.classList.remove('dark');
+        document.body.classList.add('light');
+    } else if (document.body.classList.contains('light')) {
+        document.body.classList.remove('light');
+        document.body.classList.add('dark');
+    }
+};
