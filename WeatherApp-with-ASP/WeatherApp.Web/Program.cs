95181981163c63363e06d3fed4c9f9b9bf8b0d7e
mikegsCoder <mikegsid@gmail.com>
2025-05-16 07:01:14 +0300

    Add WeatherService into IOC and exception handler

--- WeatherApp-with-ASP/WeatherApp.Web/Program.cs
+++ WeatherApp-with-ASP/WeatherApp.Web/Program.cs
@@ -1,3 +1,6 @@
+using WeatherApp.Core.Contracts;
+using WeatherApp.Core.Services;
+
 namespace WeatherApp.Web
 {
     public class Program
@@ -9,12 +12,14 @@ namespace WeatherApp.Web
             // Add services to the container.
             builder.Services.AddControllersWithViews();
             builder.Services.AddHttpClient();
+            builder.Services.AddSingleton(typeof(IWeatherService), typeof(WeatherService));
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
+                app.UseExceptionHandler("/Weather/Error");
                 app.UseHsts();
             }
 
