088afa28f883f212dd7d7517f992c19155510017
mikegsCoder <mikegsid@gmail.com>
2025-05-24 07:38:15 +0300

    Add UrlConstants

---
+++ WeatherApp-with-WPF/WeatherApp.Core/Constants/UrlConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Core.Constants
+{
+    public static class UrlConstants
+    {
+        public const string DataProviderUrl = "https://openweathermap.org/";
+        public const string SourceCodeUrl = "https://github.com/mikegsCoder/Weather-App/";
+        public const string WpfUrl = "https://learn.microsoft.com/en-us/dotnet/desktop/wpf/";
+    }
+}
