f4a90572bfe2d4be9ae98d6a40b39301d0ff904b
mikegsCoder <mikegsid@gmail.com>
2025-05-28 06:26:01 +0300

    Add Clouds.cs

---
+++ WeatherApp-with-WPF/WeatherApp.Core/Models/DTO/WeatherInfoProps/Clouds.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Core.Models.DTO.WeatherInfoProps
+{
+    public class Clouds
+    {
+        public double All { get; set; }
+    }
+}
