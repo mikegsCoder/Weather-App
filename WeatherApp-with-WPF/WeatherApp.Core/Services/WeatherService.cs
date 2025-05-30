7b190bffe2bd34e8061e2415660a03024d4c9d37
mikegsCoder <mikegsid@gmail.com>
2025-05-30 06:26:22 +0300

    Add WeatherService

---
+++ WeatherApp-with-WPF/WeatherApp.Core/Services/WeatherService.cs
@@ -0,0 +1,38 @@
+﻿using WeatherApp.Core.Contracts;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using WeatherApp.Core.Models.DTO;
+using System.Globalization;
+using WeatherApp.Core.Constants;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace WeatherApp.Core.Services
+{
+    public class WeatherService : IWeatherService
+    {
+        private readonly HttpClient client;
+
+        public WeatherService(IServiceProvider services)
+        {
+            client = services.GetRequiredService<HttpClient>();
+        }
+
+        public async Task<WeatherInfoModel> GetWeatherInfoAsync(string cityName)
+        {
+            var url = ApiConstants.BaseUrl + "weather?q=" + cityName + "&appid=" + ApiConstants.ApiKey;
+
+            string json = await client.GetStringAsync(url);
+            WeatherInfoModel weatherInfo = JsonConvert.DeserializeObject<WeatherInfoModel>(json)!;
+
+            return weatherInfo;
+        }
+    }
+}
