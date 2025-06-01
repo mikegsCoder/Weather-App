86e2fcd6bda706af77678dfb681c671c58634dbd
mikegsCoder <mikegsid@gmail.com>
2025-06-01 08:11:06 +0300

    Add FileService

---
+++ WeatherApp-with-WPF/WeatherApp.Core/Services/FileService.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
+using WeatherApp.Core.Contracts;
+using WeatherApp.Core.Models.DTO;
+
+namespace WeatherApp.Core.Services
+{
+    public class FileService : IFileService
+    {
+        public async void ExportWeatherDataAsync(WeatherInfoModel weatherData, string path, string format)
+        {
+            var result = SerializeData(weatherData, format);
+
+            await File.WriteAllTextAsync(path + "/WeatherData." + format, result);
+        }
+
+        private string SerializeData(WeatherInfoModel weatherData, string format)
+        {
+            string result;
+
+            if (format == "json")
+            {
+                result = JsonConvert.SerializeObject(weatherData, Newtonsoft.Json.Formatting.Indented);
+            }
+            else
+            {
+                var sb = new StringBuilder();
+
+                var serializer = new XmlSerializer(typeof(WeatherInfoModel), new XmlRootAttribute("WeatherData"));
+                serializer.Serialize(new StringWriter(sb), weatherData, new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty }));
+                result = sb.ToString();
+            }
+
+            return result;
+        }
+    }
+}
