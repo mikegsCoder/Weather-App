f32c4f02ca39a2d53d04e843542bb164625f5fd5
mikegsCoder <mikegsid@gmail.com>
2025-06-02 06:38:29 +0300

    Add GeneralInfoModel

---
+++ WeatherApp-with-WPF/WeatherApp.Core/Models/ViewModels/GeneralInfoViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Core.Models.ViewModels
+{
+    public class GeneralInfoViewModel
+    {
+        public string Icon { get; set; } = null!;
+        public string NameAndCountry { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string Temperature { get; set; } = null!;
+        public string FeelsLike { get; set; } = null!;
+    }
+}
