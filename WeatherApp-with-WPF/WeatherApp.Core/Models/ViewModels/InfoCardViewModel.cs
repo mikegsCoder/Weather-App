c5ea9c6638626d541fb280f79e9394063085b83c
mikegsCoder <mikegsid@gmail.com>
2025-06-02 06:39:56 +0300

    Add InfoCardViewModel

---
+++ WeatherApp-with-WPF/WeatherApp.Core/Models/ViewModels/InfoCardViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Core.Models.ViewModels
+{
+    public class InfoCardViewModel
+    {
+        public string Icon { get; set; } = null!;
+        public string Content { get; set; } = null!;
+
+        //public double Value { get; set; }
+        //public string Units { get; set; } = null!;
+        //public string Text { get; set; } = null!;
+    }
+}
