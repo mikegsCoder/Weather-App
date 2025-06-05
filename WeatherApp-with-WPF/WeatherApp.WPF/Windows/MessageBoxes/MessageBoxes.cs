b5e497f513a02bd3db37ae36d51553233f8886d7
mikegsCoder <mikegsid@gmail.com>
2025-06-05 06:33:53 +0300

    Add MessageBoxes

---
+++ WeatherApp-with-WPF/WeatherApp.WPF/Windows/MessageBoxes/MessageBoxes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WeatherApp.WPF.Windows.MessageBoxes
+{
+    public static class MessageBoxes
+    {
+        public static void ShowError(string message)
+        {
+            MessageBox.Show(
+                message,
+                "Error",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+        }
+    }
+}
