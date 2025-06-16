f73ec8643c1f386aaf537544f27be4bbbf6c9980
mikegsCoder <mikegsid@gmail.com>
2025-06-16 10:15:05 +0300

    Fixes

--- WeatherApp-with-VueJS/src/dataProviders/weather.js
+++ WeatherApp-with-VueJS/src/dataProviders/weather.js
@@ -1,10 +1,8 @@
 import { axiosInstance } from './axios';
 import { apiKey } from '../constants/appConstants';
 
-const env_apiKey = import.meta.env.API_KEY;
-
 export async function getWeatherData(cityName) {
-  const apiURL = `weather?q=${cityName}&appid=${env_apiKey || apiKey}`;
+  const apiURL = `weather?q=${cityName}&appid=${apiKey}`;
 
   try {
     const res = await axiosInstance.get(apiURL);
