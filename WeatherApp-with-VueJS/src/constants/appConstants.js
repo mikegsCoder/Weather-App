4bbb4d94a1a49b61e9e2b16e67c0ee1e2ca67cb9
mikegsCoder <mikegsid@gmail.com>
2025-06-16 09:17:58 +0300

    Fixed api key

--- WeatherApp-with-VueJS/src/constants/appConstants.js
+++ WeatherApp-with-VueJS/src/constants/appConstants.js
@@ -1,4 +1,6 @@
-const apiKey =  process.env.APIKEY ||'Your-API-Key';
+const env_apiKey = import.meta.env.API_KEY;
+
+const apiKey =  env_apiKey ||'Your-API-Key';
 const logo = './img/logo.gif';
 const baseUrl =  'https://api.openweathermap.org/data/2.5/';
 const sourceCodeUrl = 'https://github.com/mikegsCoder/Weather-App/';
