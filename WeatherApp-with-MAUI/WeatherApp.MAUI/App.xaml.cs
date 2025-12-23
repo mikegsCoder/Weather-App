3d28de8f4f908b1f7fc35f3835973878991d604c
mikegsCoder <mikegsid@gmail.com>
2025-12-23 09:49:23 +0200

    Add WetherApp with MAUI initial commit

---
+++ WeatherApp-with-MAUI/WeatherApp.MAUI/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace WeatherApp.MAUI
+{
+    public partial class App : Application
+    {
+        MainPage page;
+
+        public App(IServiceProvider services)
+        {
+            InitializeComponent();
+
+            page = services.GetService<MainPage>()!;
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage());
+        }
+    }
+}
---
+++ WeatherApp-with-MAUI/WeatherApp.MAUI/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Storage;
+using Microsoft.Maui.Controls;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading;
+
+namespace WeatherApp.MAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
