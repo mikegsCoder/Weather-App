82ce1f924b6887164e846dcf5c0217fb61b4e993
mikegsCoder <mikegsid@gmail.com>
2026-01-02 10:30:26 +0200

    Add ApiConstants

---
+++ WeatherApp-with-MAUI/WeatherApp.Core/Constants/ApiConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Core.Constants
+{
+    public static class ApiConstants
+    {
+        public const string ApiKey = "Your-Api-Key";
+
+        public const string BaseUrl = "https://api.openweathermap.org/data/2.5/";
+    }
+}
