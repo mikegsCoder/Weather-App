8f372632972443875e6d40a90af85d1e44adce9d
mikegsCoder <mikegsid@gmail.com>
2026-01-05 06:39:14 +0200

    Add WeatherService

---
+++ WeatherApp-with-MAUI/WeatherApp.Core/Services/WeatherService.cs
@@ -0,0 +1,23 @@
+﻿using WeatherApp.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using WeatherApp.Core.Models.DTO;
+using System.Globalization;
+using WeatherApp.Core.Constants;
+
+namespace WeatherApp.Core.Services
+{
+    public class WeatherService : IWeatherService
+    {
+        public async Task<WeatherInfoModel> GetWeatherInfoAsync(string cityName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
