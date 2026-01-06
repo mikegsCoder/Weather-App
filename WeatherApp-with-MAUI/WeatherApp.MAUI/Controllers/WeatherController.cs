77a68abeed8338d2dd6a18b70dadc7a882b83416
mikegsCoder <mikegsid@gmail.com>
2026-01-06 06:32:20 +0200

    Add WeatherService

--- WeatherApp-with-MAUI/WeatherApp.MAUI/Controllers/WeatherController.cs
+++ WeatherApp-with-MAUI/WeatherApp.MAUI/Controllers/WeatherController.cs
@@ -6,16 +6,20 @@ using System.Text;
 using System.Threading.Tasks;
 using WeatherApp.MAUI.DataContexts;
 using System.Globalization;
+using WeatherApp.Core.Contracts;
 
 namespace WeatherApp.MAUI.Controllers
 {
     public class WeatherController
     {
         private readonly MainPageContext context;
+        private readonly IWeatherService weatherService;
 
         public WeatherController(IServiceProvider services)
         {
+            weatherService = services.GetService<IWeatherService>()!;
             context = services.GetService<MainPageContext>()!;
         }
+
     }
 }
