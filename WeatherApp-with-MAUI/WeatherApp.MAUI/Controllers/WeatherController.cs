56378346f309cedd60dec169afcc41425edd8cc9
mikegsCoder <mikegsid@gmail.com>
2026-01-09 06:38:32 +0200

    Implemented GetWeatherInfo

--- WeatherApp-with-MAUI/WeatherApp.MAUI/Controllers/WeatherController.cs
+++ WeatherApp-with-MAUI/WeatherApp.MAUI/Controllers/WeatherController.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using WeatherApp.MAUI.DataContexts;
 using System.Globalization;
 using WeatherApp.Core.Contracts;
+using WeatherApp.Core.Models.ViewModels;
+using WeatherApp.Core.Models.DTO;
 
 namespace WeatherApp.MAUI.Controllers
 {
@@ -21,7 +23,40 @@ namespace WeatherApp.MAUI.Controllers
             context = services.GetService<MainPageContext>()!;
         }
 
-        public void GetWeatherInfo(string cityName)
+        public async void GetWeatherInfo(string cityName)
+        {
+            context.HasCity = false;
+            context.NotFound = false;
+            context.Loading = true;
+            context.City = cityName;
+
+            try
+            {
+                var weatherInfo = await weatherService.GetWeatherInfoAsync(cityName);
+
+                context.WeatherData = weatherInfo;
+                context.GeneralInfo = CreateGeneralInfo(weatherInfo); ;
+                context.InfoCards = CreateInfoCards(weatherInfo);
+                context.HasCity = true;
+            }
+            catch (Exception ex)
+            {
+                context.GeneralInfo = null;
+                context.InfoCards = null;
+                context.NotFound = true;
+            }
+            finally
+            {
+                context.Loading = false;
+            }
+        }
+
+        private List<InfoCardViewModel> CreateInfoCards(WeatherInfoModel weatherInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        private GeneralInfoViewModel CreateGeneralInfo(WeatherInfoModel weatherInfo)
         {
             throw new NotImplementedException();
         }
