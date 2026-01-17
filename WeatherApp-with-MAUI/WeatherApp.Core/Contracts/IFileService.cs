221d5f2ba2e25ae7d0b5c1fdc8551ef087b989ec
mikegsCoder <mikegsid@gmail.com>
2026-01-17 11:09:10 +0200

    Add IFileService

---
+++ WeatherApp-with-MAUI/WeatherApp.Core/Contracts/IFileService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Core.Models.DTO;
+using WeatherApp.Core.Models.ViewModels;
+
+namespace WeatherApp.Core.Contracts
+{
+    public interface IFileService
+    {
+        void ExportWeatherDataAsync(WeatherInfoModel weatherData, string path, string format);
+    }
+}
