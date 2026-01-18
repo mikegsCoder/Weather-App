9478c80a8f9f30b645795365d30298a8b500cbde
mikegsCoder <mikegsid@gmail.com>
2026-01-18 10:17:09 +0200

    Add MessageConstants

---
+++ WeatherApp-with-MAUI/WeatherApp.Core/Constants/MessageConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Core.Constants
+{
+    public static class MessageConstants
+    {
+        public const string FolderSelectionError = "Invalid folder selection!";
+        public const string DataExportError = "Data export failed!";
+        public const string DataExportSuccess = "Weather data exported successfully to {0}WeatherData.{1}";
+    }
+}
