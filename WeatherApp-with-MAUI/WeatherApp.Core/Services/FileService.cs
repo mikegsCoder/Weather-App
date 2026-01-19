09a267ec20f467dccf00fd23aed233d08f1c1b1d
mikegsCoder <mikegsid@gmail.com>
2026-01-19 06:22:15 +0200

    Implemented ExportWeatherDataAsync

--- WeatherApp-with-MAUI/WeatherApp.Core/Services/FileService.cs
+++ WeatherApp-with-MAUI/WeatherApp.Core/Services/FileService.cs
@@ -15,6 +15,13 @@ namespace WeatherApp.Core.Services
     {
         public async void ExportWeatherDataAsync(WeatherInfoModel weatherData, string path, string format)
         {
+            var result = SerializeData(weatherData, format);
+
+            await File.WriteAllTextAsync(path + "/WeatherData." + format, result);
+        }
+
+        private string? SerializeData(WeatherInfoModel weatherData, string format)
+        {
             throw new NotImplementedException();
         }
     }
