df0c5232b53e78111fd6be3112d5e4410f49a026
mikegsCoder <mikegsid@gmail.com>
2026-01-26 07:03:09 +0200

    Add WeatherApp with Next initial commit

---
+++ WeatherApp-with-Next/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Weather App",
+  description: "Weather App with NEXT",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ WeatherApp-with-Next/app/page.tsx
@@ -0,0 +1,10 @@
+'use server'
+
+export default async function Home() {
+
+  return (
+    <div id="root">
+      <h2>Weather App</h2>
+    </div>
+  );
+}
