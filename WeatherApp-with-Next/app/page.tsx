2697014a1efbe22ce9bcc1fd4ee71b8379cf97ff
mikegsCoder <mikegsid@gmail.com>
2026-01-27 06:50:39 +0200

    Add ErrorBoundary

--- WeatherApp-with-Next/app/page.tsx
+++ WeatherApp-with-Next/app/page.tsx
@@ -1,10 +1,14 @@
 'use server'
 
+import ErrorBoundary from '@/components/ErrorBoundary';
+
 export default async function Home() {
 
   return (
     <div id="root">
-      <h2>Weather App</h2>
+      <ErrorBoundary>
+        <h2>Weather App</h2>
+      </ErrorBoundary>
     </div>
   );
 }
