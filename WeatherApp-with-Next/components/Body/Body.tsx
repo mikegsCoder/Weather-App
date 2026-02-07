4b7d3b3215bfe106d08ab336fd1095c91c1d2e86
mikegsCoder <mikegsid@gmail.com>
2026-02-07 11:04:58 +0200

    Add router, pathname and searchParams

--- WeatherApp-with-Next/components/Body/Body.tsx
+++ WeatherApp-with-Next/components/Body/Body.tsx
@@ -1,5 +1,7 @@
 'use client'
-import { ReactElement } from 'react';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
+import { ReactElement, useState, useTransition } from 'react';
 import { useCityContext } from '@/contexts/CityContext';
 
 import IResponseData from '@/interfaces/IResponseData';
@@ -8,6 +10,15 @@ const Body = (props: {
   data: IResponseData,
 }): ReactElement => {
   const { city } = useCityContext();
+
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const [isPending, startTransition] = useTransition();
+  const [inputChanges, setInputChanges] = useState(false);
+  const [notFound, setNotFound] = useState(false);
+  const [infoCards, setInfoCards] = useState([] as ReactElement[]);
 
   return (
     <div className="">
