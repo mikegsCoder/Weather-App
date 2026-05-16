4fc358e91ecd54224bfaa63839346086f40e0713
mikegsCoder <mikegsid@gmail.com>
2026-05-16 10:51:52 +0300

    Add WeatherApp with Blazor Interactive Server initial commit

---
+++ WeatherApp-with-Blazor/Interactive-Server/WeatherApp.Web/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿// Write your JavaScript code.
+
+document.body.classList.add('light');
+
+const input = document.getElementById('cityInput');
+
+function toggleTheme() {
+    if (document.body.classList.contains('dark')) {
+        document.body.classList.remove('dark');
+        document.body.classList.add('light');
+    } else if (document.body.classList.contains('light')) {
+        document.body.classList.remove('light');
+        document.body.classList.add('dark');
+    }
+};
+
+function clearBody() {
+    const weatherInfo = document.getElementById('weather-info');
+    if (weatherInfo) weatherInfo.style.display = 'none';
+
+    const notFound = document.getElementById('not-found');
+    if (notFound) notFound.style.display = 'none';
+}
