78acd62e522defd67edd02bd83db983525d3f4d3
mikegsCoder <mikegsid@gmail.com>
2026-05-20 06:54:37 +0300

    Add NotFound component logic

---
+++ WeatherApp-with-Blazor/Interactive-Server/WeatherApp.Web/Components/NotFound.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace WeatherApp.Web.Components
+{
+    public partial class NotFound
+    {
+        [Parameter]
+        public string? CityName { get; set; }
+    }
+}
