f8427f3eb901ba07cfd0ca78a58849dfa941179f
mikegsCoder <mikegsid@gmail.com>
2026-05-23 09:14:38 +0300

    Add Sys

---
+++ WeatherApp-with-Blazor/Interactive-Server/WeatherApp.Core/Models/DTO/WeatherInfoProps/Sys.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Core.Models.DTO.WeatherInfoProps
+{
+    public class Sys
+    {
+        public double Type { get; set; }
+        public double Id { get; set; }
+        public string Country { get; set; } = null!;
+        public double Sunrise { get; set; }
+        public double Sunset { get; set; }
+    }
+}
