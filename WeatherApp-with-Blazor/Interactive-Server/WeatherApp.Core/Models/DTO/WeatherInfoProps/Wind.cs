c47a3560baa9f1077537f4c04b5fc510c7d72337
mikegsCoder <mikegsid@gmail.com>
2026-05-24 09:57:38 +0300

    Add Wind model

---
+++ WeatherApp-with-Blazor/Interactive-Server/WeatherApp.Core/Models/DTO/WeatherInfoProps/Wind.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Core.Models.DTO.WeatherInfoProps
+{
+    public class Wind
+    {
+        public double Speed { get; set; }
+        public double Deg { get; set; }
+    }
+}
