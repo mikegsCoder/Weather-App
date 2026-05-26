97bd7759711b0a617a2a5d838ef921737187f995
mikegsCoder <mikegsid@gmail.com>
2026-05-26 06:44:40 +0300

    Add WeatherService and dependencies

---
+++ WeatherApp-with-Blazor/Interactive-Server/WeatherApp.Core/Services/WeatherService.cs
@@ -0,0 +1,25 @@
+﻿using WeatherApp.Core.Contracts;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using WeatherApp.Core.Models.DTO;
+using WeatherApp.Core.Models.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WeatherApp.Core.Services
+{
+    public class WeatherService : IWeatherService
+    {
+        public Task<Tuple<GeneralInfoViewModel, List<InfoCardViewModel>>> GetWeatherInfoAsync(string cityName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
