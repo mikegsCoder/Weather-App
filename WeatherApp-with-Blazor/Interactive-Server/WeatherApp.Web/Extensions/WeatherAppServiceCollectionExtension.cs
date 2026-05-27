f780f91b02997fdfbfdf5ec6a3d4388fed252e62
mikegsCoder <mikegsid@gmail.com>
2026-05-27 06:46:41 +0300

    Add service collection extension

---
+++ WeatherApp-with-Blazor/Interactive-Server/WeatherApp.Web/Extensions/WeatherAppServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using WeatherApp.Core.Contracts;
+using WeatherApp.Core.Services;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class WeatherAppServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IWeatherService), typeof(WeatherService));
+            services.AddScoped<HttpClient>();
+
+            return services;
+        }
+    }
+}
