c579ea288ea7359cc2990e25bf9f37a7c18e9ee7
mikegsCoder <mikegsid@gmail.com>
2026-05-30 09:45:44 +0300

    Add CityInput interaction logic

---
+++ WeatherApp-with-Blazor/Interactive-Server/WeatherApp.Web/Components/CityInput.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace WeatherApp.Web.Components
+{
+    public partial class CityInput
+    {
+        [Parameter]
+        public EventCallback<string> OnSubmitCallback { get; set; }
+
+        private string city = string.Empty;
+
+        public string City
+        {
+            get => city;
+            set
+            {
+                if (value == city) return;
+
+                city = value;
+            }
+        }
+
+        private async Task SendData()
+        {
+            await OnSubmitCallback.InvokeAsync(City);
+        }
+    }
+}
