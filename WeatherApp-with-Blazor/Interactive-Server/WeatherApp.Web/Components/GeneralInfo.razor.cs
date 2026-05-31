fd7c6c45375c6885628dea6ec66e949da3a26e29
mikegsCoder <mikegsid@gmail.com>
2026-05-31 10:07:19 +0300

    Add GeneralInfoModel

--- WeatherApp-with-Blazor/Interactive-Server/WeatherApp.Web/Components/GeneralInfo.razor.cs
+++ WeatherApp-with-Blazor/Interactive-Server/WeatherApp.Web/Components/GeneralInfo.razor.cs
@@ -1,5 +1,11 @@
-﻿namespace WeatherApp.Web.Components
+﻿using Microsoft.AspNetCore.Components;
+using WeatherApp.Core.Models.ViewModels;
+
+namespace WeatherApp.Web.Components
 {
     public partial class GeneralInfo
-    {}
+    {
+        [Parameter]
+        public GeneralInfoViewModel? GeneralInfoModel { get; set; }
+    }
 }
