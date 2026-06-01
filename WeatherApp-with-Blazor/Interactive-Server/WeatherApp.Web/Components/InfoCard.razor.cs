2de3c6533b743870c276c1065bfb9a6f7435a19f
mikegsCoder <mikegsid@gmail.com>
2026-06-01 06:21:58 +0300

    Add InfoCardModel

--- WeatherApp-with-Blazor/Interactive-Server/WeatherApp.Web/Components/InfoCard.razor.cs
+++ WeatherApp-with-Blazor/Interactive-Server/WeatherApp.Web/Components/InfoCard.razor.cs
@@ -1,5 +1,11 @@
-﻿namespace WeatherApp.Web.Components
+﻿using Microsoft.AspNetCore.Components;
+using WeatherApp.Core.Models.ViewModels;
+
+namespace WeatherApp.Web.Components
 {
     public partial class InfoCard
-    {}
+    {
+        [Parameter]
+        public InfoCardViewModel? InfoCardModel { get; set; }
+    }
 }
