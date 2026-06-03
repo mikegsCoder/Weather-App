5c4e4ca11edc64a0bec29fb8dbac9bea9e7113b3
mikegsCoder <mikegsid@gmail.com>
2026-06-03 06:49:10 +0300

    Add Submit method

--- WeatherApp-with-Blazor/Interactive-Server/WeatherApp.Web/Components/Body.razor.cs
+++ WeatherApp-with-Blazor/Interactive-Server/WeatherApp.Web/Components/Body.razor.cs
@@ -27,5 +27,32 @@ namespace WeatherApp.Web.Components
 
         public string? City { get; set; }
 
+        public async Task Submit(string cityName)
+        {
+            City = cityName;
+
+            logger.LogInformation("WeatherForm submitted with cityname = " + cityName);
+
+            var result = await GetWeatherInfo(cityName);
+
+            if (result == null)
+            {
+                notFound = true;
+                hasData = false;
+            }
+            else
+            {
+                notFound = false;
+                hasData = true;
+
+                GeneralInfo = result.Item1;
+                InfoCards = result.Item2;
+            }
+        }
+
+        private async Task<Tuple<GeneralInfoViewModel, List<InfoCardViewModel>>> GetWeatherInfo(string cityName)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
