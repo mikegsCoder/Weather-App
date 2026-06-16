28adf3337eea4a6898e6e0be875aaf1b79c3bcc1
mikegsCoder <mikegsid@gmail.com>
2026-06-16 06:49:40 +0300

    Add InfoCardViewModel

---
+++ WeatherApp-with-Blazor/Web-Assembly/WeatherApp.Core/Models/ViewModels/InfoCardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Core.Models.ViewModels
+{
+    public class InfoCardViewModel
+    {
+        public string Icon { get; set; } = "";
+        public double Value { get; set; } = 0;
+        public string Units { get; set; } = "";
+        public string Text { get; set; } = "";
+    }
+}
