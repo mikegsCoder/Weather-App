366f80e6b950411eaece0bbd43a88d362d0c408d
mikegsCoder <mikegsid@gmail.com>
2026-06-16 06:48:00 +0300

    Add GeneralInfoViewModel

---
+++ WeatherApp-with-Blazor/Web-Assembly/WeatherApp.Core/Models/ViewModels/GeneralInfoViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Core.Models.ViewModels
+{
+    public class GeneralInfoViewModel
+    {
+        public string Icon { get; set; } = "";
+        public string Name { get; set; } = "";
+        public string Country { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string Temp { get; set; } = "";
+        public string FeelsLike { get; set; } = "";
+    }
+}
