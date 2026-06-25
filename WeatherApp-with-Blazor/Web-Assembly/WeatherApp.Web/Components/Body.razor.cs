d6ecc8d7711e3e6a8bfbe93633d7870b2cdff005
mikegsCoder <mikegsid@gmail.com>
2026-06-25 06:24:48 +0300

    Add Body component

---
+++ WeatherApp-with-Blazor/Web-Assembly/WeatherApp.Web/Components/Body.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics.Metrics;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using WeatherApp.Core.Models.ViewModels;
+
+namespace WeatherApp.Web.Components
+{
+    public partial class Body
+    {
+        private bool isLoading = false;
+        private bool notFound = false;
+        private bool hasData = false;
+
+        GeneralInfoViewModel GeneralInfo = new();
+        List<InfoCardViewModel> InfoCards = new();
+
+        public string? City { get; set; }
+
+        public async Task Submit( string cityName)
+        {
+        }
+    }
+}
