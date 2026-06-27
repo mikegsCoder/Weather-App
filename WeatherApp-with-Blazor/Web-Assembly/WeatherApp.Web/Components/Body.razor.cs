ad5ff52d6753e1f5baf720c3454e2a503b7b79c4
mikegsCoder <mikegsid@gmail.com>
2026-06-27 10:10:20 +0300

    Code cleanup

--- WeatherApp-with-Blazor/Web-Assembly/WeatherApp.Web/Components/Body.razor.cs
+++ WeatherApp-with-Blazor/Web-Assembly/WeatherApp.Web/Components/Body.razor.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.AspNetCore.Components;
-using Microsoft.Extensions.Logging;
-using System.Diagnostics.Metrics;
-using System.Net.NetworkInformation;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
 using WeatherApp.Core.Contracts;
 using WeatherApp.Core.Models.ViewModels;
 
