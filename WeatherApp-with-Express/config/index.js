c4f627d8629f47e96ca3bfbc0b5a798108f4eba2
mikegsCoder <mikegsid@gmail.com>
2026-07-06 07:14:26 +0300

    Add app config

---
+++ WeatherApp-with-Express/config/index.js
@@ -0,0 +1,8 @@
+const apiKey = 'Your-Api-Key';
+const baseUrl = 'https://api.openweathermap.org/data/2.5/';
+
+module.exports = {
+  apiKey,
+  baseUrl,
+  port: 3000,
+};
--- WeatherApp-with-Express/index.js
+++ WeatherApp-with-Express/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const port = 3000;
+const { port } = require('./config');
 const expressConfig = require('./config/express');
 
 start();
