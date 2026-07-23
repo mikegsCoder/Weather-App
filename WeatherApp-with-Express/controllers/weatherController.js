8f8b307f3f068e46df308456edb4f433e33b9835
mikegsCoder <mikegsid@gmail.com>
2026-07-23 07:00:41 +0300

    Add router.post

--- WeatherApp-with-Express/controllers/weatherController.js
+++ WeatherApp-with-Express/controllers/weatherController.js
@@ -5,6 +5,24 @@ const { getWeatherData } = require('../services/weather');
 const { createGeneralData } = require('../utils/createGeneralData');
 const { createInfoCardsData } = require('../utils/createInfoCardsData');
 
-// To implement
+const jobs = {};
+
+router.post('/', (req, res) => {
+  const id = crypto.randomUUID();
+
+  jobs[id] = {
+    status: 'loading',
+    data: null
+  };
+
+  // starting async task:
+  loadWeather(id, req.body.city);
+
+  res.redirect('/weather/' + id);
+});
+
+async function loadWeather(id, city) {
+  // To implement
+}
 
 module.exports = router;
