c1a2d27eaa349e7d39c3835efffff9ce19192a37
mikegsCoder <mikegsid@gmail.com>
2026-07-29 06:54:39 +0300

    Add handleInputChange and clearBody

--- WeatherApp-with-Express/static/js/script.js
+++ WeatherApp-with-Express/static/js/script.js
@@ -5,6 +5,19 @@ document.getElementById('checkbox').addEventListener('click', toggleTheme);
 document.getElementById('input').addEventListener('input', handleInputChange);
 document.getElementById('inputForm').addEventListener('submit', handleSubmit);
 
+function handleInputChange(e) {
+  city = e.target.value;
+  clearBody();
+}
+
+function clearBody() {
+  const weatherInfo = document.getElementById('weather-info');
+  if (weatherInfo) weatherInfo.style.display = 'none';
+
+  const notFound = document.getElementById('not-found');
+  if (notFound) notFound.style.display = 'none';
+}
+
 function toggleTheme() {
   if (document.body.classList.contains('dark')) {
     document.body.classList.remove('dark');
